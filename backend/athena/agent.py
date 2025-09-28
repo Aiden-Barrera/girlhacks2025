338d5064ca62f11d52537f4cf9192f463debf592
FardeenI <fi43@njit.edu>
2025-09-28 05:18:19 -0400

    Endpoints with agents?

--- backend/athena/agent.py
+++ backend/athena/agent.py
@@ -1,7 +1,9 @@
-from strands import Agent, tool
+from strands import Agent
 # from strands_tools import calculator, current_time
 from dotenv import load_dotenv
 import os
+import json
+import requests
 
 # Load environment variables from .env file
 load_dotenv('../.env')
@@ -13,8 +15,31 @@ agent = Agent(
 )
 
 # Ask the agent a question that uses the available tools
-message = """
-Hey I'm a complete beginner, how do I become a data scientist?
-"""
+message = os.getenv("USER_MESSAGE")
 
-agent(message)
+response = agent(message)
+response_str = str(response)
+
+# Find JSON boundaries
+start = response_str.find('{')
+end = response_str.rfind('}') + 1
+
+json_str = response_str[start:end]
+processedData = json.loads(json_str)
+
+def parse_mermaid_from_llm(llm_output):
+    """
+    Accept the Roadmap Agent's output and parse out the mermaid code block into a string with proper syntax for Mermaid JS
+
+    Args:
+        llm_output (str): The output from the Roadmap Agent
+
+    Returns:
+        str: A parsed block of mermaid JS code
+    """
+    return llm_output.replace('\\n', '\n').replace('\\"', '"').strip()
+
+
+response = requests.post('http://localhost:3000/api/pathways', json=processedData['outputs'])
+print(response.text)
+print(response.status_code)
