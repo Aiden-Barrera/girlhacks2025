44dc1931895f437bb1a009ab03a8e63634e67179
JiaPBlake <jpb72@njit.edu>
2025-09-27 20:07:32 -0400

    First commit to allow for frontend branch merging

---
+++ frontend-Jia/jia-react-app/src/Components-Jia/AboutUs.jsx
@@ -0,0 +1,7 @@
+
+function AboutUs() {
+    return(
+    <p style={{display:'flex', justifyContent:'center', alignItems:'center'}}>Career Pathway is a personalized list of steps you can take to get on track towards your desired career!</p>
+    );
+}
+export default AboutUs
---
+++ frontend-Jia/jia-react-app/src/Components-Jia/Title.jsx
@@ -0,0 +1,15 @@
+// New component for header
+
+function Title(){
+
+    //within the return statement, you can write pure HTML
+    return(
+        <header>
+            <h1 style={{display:'flex', justifyContent:'center', alignItems:'center'}}>Career Pathway</h1>
+            <hr></hr>
+        </header>
+    );
+
+}
+
+export default Title
